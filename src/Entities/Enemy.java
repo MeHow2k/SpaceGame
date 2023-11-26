a43e3390e289e3fc6dd52d5188e37b599829c6da
Daniel <26562@student.kpu.krosno.pl>
2023-11-26 23:12:24 +0100

    Klasa Enemy

--- src/Entities/Enemy.java
+++ src/Entities/Enemy.java
@@ -1,1 +1,60 @@
+//klasa reprezentująca wroga
 
+package Entities;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Enemy extends Thread{
+
+    private int x=0,y=0,w=50,h=50,speed=1,score_increment=10;
+    JPanel panel;
+
+    //konstruktor
+    public Enemy(int x, int y, JPanel panel){
+        this.x=x;
+        this.y=y;
+        this.panel=panel;
+    }
+
+    //metoda rysujaca obiekt
+    public void draw(Graphics2D g){
+        g.setColor(Color.blue);
+        g.fillRect(x,y,w,h);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+
+            //y=y+speed;
+            x=x+speed;
+
+            try {
+                sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
