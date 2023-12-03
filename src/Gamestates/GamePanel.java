7d44f00f7ca771cfdd7eacaa662f44b6fc0f9826
Pawel <pawelsanocki@outlook.com>
2023-12-03 20:52:11 +0100

    Dodanie obsługi sterowania graczem, dodanie systemu odtwarzania audio.

--- src/Gamestates/GamePanel.java
+++ src/Gamestates/GamePanel.java
@@ -6,11 +6,15 @@ import Constants.C;
 import Entities.Enemy;
 import Entities.Player;
 import Entities.Points;
+import Manager.SoundManager;
 
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class GamePanel extends JPanel implements KeyListener {
@@ -21,10 +25,13 @@ public class GamePanel extends JPanel implements KeyListener {
     //tu będą listy obiektów
     ArrayList<Enemy> listEnemy = new ArrayList(20);//lista wrogow
 
+    boolean LEFT_PRESSED, RIGHT_PRESSED, DOWN_PRESSED, UP_PRESSED;
+
     GamePanel(){
         super(null);
         Color color = new Color(132, 142, 220);
         setBackground(color);
+        addKeyListener(this);
         //deklaracja obiektow
         player= new Player(200,200);
         points= new Points(30,0,this);
@@ -32,6 +39,12 @@ public class GamePanel extends JPanel implements KeyListener {
         newEnemy(100,100,50,50);
         newEnemy(200,100,50,50);
         newEnemy(300,100,50,50);
+        //Odtworzenie głównego motywu gry
+        try {
+            SoundManager.playBackground();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         new Thread(new Runnable() {
             @Override
@@ -54,8 +67,36 @@ public class GamePanel extends JPanel implements KeyListener {
 
                     //glowny watek gry- mechaniki itd.
                     if (C.GAMESTATE==0){
-                        player.moveX(1);//test ruchu klasy gracza
-                       // System.out.println("x:"+player.getX()+" y:"+player.getY() );//test up
+                        //obsluga ruchu gracza
+                        if (RIGHT_PRESSED == true) {
+                            if (C.FRAME_WIDTH - 60 >= player.getX()) {
+                                player.moveX(1); //poruszanie się w prawo
+                            }
+                            if (C.FRAME_WIDTH - 60 <= player.getX()) {
+                                player.moveX(-5); //odbijanie się od krawędzi
+                            }
+                        }
+                        if (LEFT_PRESSED == true) {
+                            if (0 <= player.getX()) {
+                                player.moveX(-1); //poruszanie się w lewo
+                            }
+                            if (0 >= player.getX()) {
+                                player.moveX(5); //odbijanie się od krawędzi
+                            }
+                        }
+                        if (DOWN_PRESSED == true) {
+                            if (C.FRAME_HEIGHT - 150 >= player.getY()) {
+                                player.moveY(1); //poruszanie się w dół
+                            }
+                        }
+                        if (UP_PRESSED == true) {
+                            if (0 < player.getY()) {
+                                player.moveY(-1); //poruszanie się w górę
+                            }
+                        }
+
+                        //player.moveX(1);//test ruchu klasy gracza
+                        //System.out.println("x:"+LEFT_PRESSED+" y:"+RIGHT_PRESSED );//test up
 
 
                     }
@@ -123,7 +164,6 @@ public class GamePanel extends JPanel implements KeyListener {
 
     }
 
-
     //keylistener do sterowania
     @Override
     public void keyTyped(KeyEvent e) {
@@ -132,11 +172,34 @@ public class GamePanel extends JPanel implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        ////   ruch gracza
+        if (e.getKeyCode()==37){//s w lewo
+            LEFT_PRESSED=true;
+        }
+        if (e.getKeyCode()==39){//s w prawo
+            RIGHT_PRESSED=true;
+        }
+        if (e.getKeyCode()==40){//s w dol
+            DOWN_PRESSED=true;
+        }
+        if (e.getKeyCode()==38){//s w gore
+            UP_PRESSED=true;
+        }
     }
-
+    //////////////////////////////////////testing////////////////////////////////////////////////////////////
     @Override
     public void keyReleased(KeyEvent e) {
-
+        if (e.getKeyCode()==37){//s w lewo
+            LEFT_PRESSED=false;
+        }
+        if (e.getKeyCode()==39){//s w prawo
+            RIGHT_PRESSED=false;
+        }
+        if (e.getKeyCode()==40){//s w dol
+            DOWN_PRESSED=false;
+        }
+        if (e.getKeyCode()==38){//s w gore
+            UP_PRESSED=false;
+        }
     }
 }
