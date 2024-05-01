25b6f69563030d3e80e091319bb762b576df6757
MeHow2k <destroyeverythingpl@gmail.com>
2024-05-01 12:28:10 +0200

    Poprawka dot Gamestate przy uruchamianiu

--- src/Constants/C.java
+++ src/Constants/C.java
@@ -8,7 +8,7 @@ public class C {
 
     public static int FRAME_HEIGHT=850;
     public static int FRAME_WIDTH=800;
-    public static int GAMESTATE=99; // 0- gra 1-menu glowne 2-menusettings 3-how to play 4- autorzy 5- wybor skinow przed gra, 100 - intro, 99 - wybor jezyka
+    public static int GAMESTATE=100; // 0- gra 1-menu glowne 2-menusettings 3-how to play 4- autorzy 5- wybor skinow przed gra, 100 - intro, 99 - wybor jezyka
     public static int LEVEL=0;
     public static int LAST_LEVEL=50;
     public static boolean GODMODE=false;
