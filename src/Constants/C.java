a5f3671b01cb3fc8b977ec3f977290092c059376
MeHow2k <destroyeverythingpl@gmail.com>
2024-05-12 11:29:04 +0200

    Version 1.0.1 dev commmit

--- src/Constants/C.java
+++ src/Constants/C.java
@@ -12,7 +12,7 @@ public class C {
     public static int FRAME_WIDTH=800;
     public static int GAMESTATE=100; // 0- gra 1-menu glowne 2-menusettings 3-how to play 4- autorzy 5- wybor skinow przed gra,
     // 22-playernameseletion,21-playersettings , 100 - intro, 99 - wybor jezyka
-    public static int LEVEL=0;
+    public static int LEVEL=10;
     public static int LAST_LEVEL=51;
     public static boolean GODMODE=false;
     public static String PLAYER_NAME="";
