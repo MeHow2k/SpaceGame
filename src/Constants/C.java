37b8a5a0484f219fa01a627306db963e07ebca9d
MeHow2k <destroyeverythingpl@gmail.com>
2024-05-25 14:11:27 +0200

    After v1.0.3 dev commit

--- src/Constants/C.java
+++ src/Constants/C.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class C {
-    public static final String VERSION= "1.0.3";//last 1.0.2
+    public static final String VERSION= "1.0.3dev";//last 1.0.2
     public static int LANGUAGE=999; //0-en 1-pl-
-    public static boolean DEVELOPMENT=false;
+    public static boolean DEVELOPMENT=true;
     public static String SECRETKEY = "fg54w3uhwa23y56tdxse45uy";
 
     public static int FRAME_HEIGHT=850;
